6f1935ff6eedbb582fd7852690da39b939581cc3
netsider <rdoubleoc@aol.com>
2015-03-12 15:39:44 -0400

    Commit: 7

--- AJAX-request-XML-getElementsByTagName.js
+++ AJAX-request-XML-getElementsByTagName.js
@@ -1,12 +1,13 @@
-//TypeError: request.responseXML is null
-//Uncaught TypeError: Cannot read property 'getElementsByTagName' of null
 window.onload = init();
 function init(){
 var request = new XMLHttpRequest();
 request.open('GET', 'data.xml');
 request.onreadystatechange = function() {	
-	if(request.status==200){
-		console.log(request.responseXML.getElementsByTagName('link')[0]); // Logs entire XML document to console in tree form
+	if((request.status===200) && (request.readyState===4)){ // was giving the following errors without (request.readyState===4):
+	//TypeError: request.responseXML is null && Uncaught TypeError: Cannot read property 'getElementsByTagName' of null
+	console.log(request.responseXML.getElementsByTagName('speaker')[0]); // Logs first element to console
+	console.log(request.responseXML.getElementsByTagName('name')[0].firstChild); // Logs the first element's <name> tags' first child to console
+	console.log(request.responseXML.getElementsByTagName('name')[0].firstChild.nodeValue); // Logs the first element's <name> tags' firstChild's nodeValue to console
 	}
 };
 request.send();
